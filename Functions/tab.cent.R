134f67dd80149f868277576e71cfb5840fa59b54
Omar Lizardo <olizardo@soc.ucla.edu>
2025-07-20 08:15:23 -0700

    new tabs

--- Functions/tab.cent.R
+++ Functions/tab.cent.R
@@ -1,11 +1,19 @@
-tab.cent <- function(x, d = 3, name = "name", caption = "", label = "label") { #table function
+tab.cent <- function(x, 
+      d = 3, 
+      name = "name", 
+      caption = "", 
+      label = "label",
+      rownames) { 
 library(kableExtra)
+library(dplyr)
+x <- distinct(x[, 2:6])
+x <- cbind(rownames, x)
 t <- kbl(x, 
       format = "latex", booktabs = TRUE, linesep = "",
       digits = d, row.names = FALSE,
-      col.names = c("Node", "Distinction", "SDistinction", "Status", "Constraint", "Scalar"),
+      col.names = c("Nodes", "Distinction", "SDistinction", "Status", "Constraint", "Scalar"),
       caption = caption, label = label
       ) %>% 
       kable_styling(bootstrap_options = c("hover", "condensed", "responsive")) %>% 
       save_kable(file = here("Tabs", paste(name, ".tex", sep = "")))
-      }
+}
