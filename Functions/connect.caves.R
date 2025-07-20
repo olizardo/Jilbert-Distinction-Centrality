af3312646491456f60c37a600435e4e53eb8abf5
Omar Lizardo <olizardo@soc.ucla.edu>
2025-07-20 07:49:04 -0700

    new stuff

--- Functions/connect.caves.R
+++ Functions/connect.caves.R
@@ -1,27 +1,27 @@
 connect.caves <- function(c = 4, nc = 4, n = 12) {
-    make.cave.list <- function(c) {
+    make.cave.list <- function(a, b) {
       caves <- list() 
       i <- 1
-      for (k in 1:nc) {
-        j <- i + (c - 1)
+      for (k in 1:b) {
+        j <- i + (a - 1)
         caves[[k]] <- c(i:j)
-        i <- i + c
+        i <- i + a
       }
     return(caves)
     } # end make.cave.list
-    caves <- make.cave.list(c)
-    make.caves <- function(c, nc) {
-      g <- make_full_graph(c, directed = FALSE)
-      for (k in 2:nc) {
-        g <- g + make_full_graph(c, directed = FALSE)
+    caves <- make.cave.list(c, nc)
+    make.caves <- function(a, b) {
+      g <- make_full_graph(a, directed = FALSE)
+      for (k in 2:b) {
+        g <- g + make_full_graph(a, directed = FALSE)
         }
       return(g) 
       } # end make.caves
-    gl <- vector(mode = "list", length = n)
     g <- make.caves(c, nc)
+    gl <- vector(mode = "list", length = n)
     gl[[1]] <- g
     for (k in 2:n) {
-      ca <- sample(1:c, 2)
+      ca <- sample(1:nc, 2)
       ca1 <- caves[[ca[1]]]
       ca2 <- caves[[ca[2]]]
       g <- add_edges(g, c(sample(ca1, 1), sample(ca2, 1)))
--- Functions/distinction.R
+++ Functions/distinction.R
@@ -1,4 +1,4 @@
-distinction <- function(x, norm = FALSE) { #distinction centrality function
+distinction <- function(x, norm = FALSE) { 
    library(igraph)
    V <- vcount(x)
    no.labels <- is.null(V(x)$name)
@@ -10,16 +10,16 @@ distinction <- function(x, norm = FALSE) { #distinction centrality function
       names <- V(x)$name 
       V(x)$name <- 1:V
       }
-   norm.max <- function(x) {return(x/max(x))}
+   norm.max <- function(x) {return(x/max(x))} #normalizing function
    A <- as.matrix(as_adjacency_matrix(x))
-   nc <- components(x)$no
+   nc <- components(x)$no #checking for disconnected graph
    if (nc > 1) {
       s <- rowSums(abs(eigen(A)$vectors[, 1:nc]))
       }
    else if (nc == 1) {
       s <- abs(eigen(A)$vectors[, 1])
       }
-   if (norm == TRUE) {s <- norm.max(s)}
+   if (norm == TRUE) {s <- norm.max(s)} #normalizing by maximum
    names(s) <- V(x)$name
    d <- rep(V, 0) # initializing distinction vector
    u <- rep(V, 0) # initializing constraint vector
@@ -33,7 +33,7 @@ distinction <- function(x, norm = FALSE) { #distinction centrality function
          A <- as.matrix(as_adjacency_matrix(x.d))
          s.a <- abs(eigen(A)$vectors[, 1])
          names(s.a) <- labs[-which(labs == i)]
-         if (norm == TRUE) {s.a <- norm.max(s.a)}
+         if (norm == TRUE) {s.a <- norm.max(s.a)} #normalizing by maximum
          s.a <- s.a[j]
          } #end main if
       else if (c == FALSE & e == TRUE) { #empty node-deleted graph
@@ -44,7 +44,7 @@ distinction <- function(x, norm = FALSE) { #distinction centrality function
          nc <- components(x.d)$no         
          s.a <- rowSums(abs(eigen(A)$vectors[, 1:nc]))
          names(s.a) <- labs[-which(labs == i)]
-         if (norm == TRUE) {s.a <- norm.max(s.a)}
+         if (norm == TRUE) {s.a <- norm.max(s.a)} #normalizing by maximum
          s.a <- s.a[j]
          } #end second else
       u[i] <- mean(s.a) #i's average neighbor eigenvector centrality in node deleted subgraph
