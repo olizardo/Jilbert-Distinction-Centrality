05bfe9255b42aa4270de57f12cf6e93af62cbb7e
Omar Lizardo <olizardo@soc.ucla.edu>
2025-07-31 10:17:53 -0700

    now new and improved

--- Functions/distinction.R
+++ Functions/distinction.R
@@ -8,17 +8,20 @@ distinction <- function(x, norm = "max", digits  = 4) {
   if (is.null(V(x)$name) == 0) {
     names <- V(x)$name 
     V(x)$name <- 1:n
-    }
+    } 
   norm.max <- function(x) {x^2/max(x^2)} #sum to one and max one normalizing function
   norm.one <- function(x) {x^2} #sum to one normalizing function
   norm.abm <- function(x) {abs(x)/max(abs(x))} #max one normalizing function
   norm.abs <- function(x) {abs(x)} #absolute value normalizing function
   eig.x <- eigen(as_adjacency_matrix(x)) #eigenvector decomposition of adjacency matrix
-  nc <- components(x)$no #number of components
-  iso <- sum(components(g)$csize == 1) #checking for isolated components
+  nc <- components(x)$no #number of components in graph
+  if (nc > 1) {
+    nn <- sum(components(x)$csize != 1) #number of non-isolated components in graph
+    ci <- which(components(x)$csize != 1) #column indices for non-isolated components
+    }
   if (nc == 1) {s <- eig.x$vectors[, 1]} #collecting status scores from first eigenvector in connected graph
-  if (nc > 1 & iso > 0) {s <- eig.x$vectors[, 1]} #collecting status scores from first eigenvector in connected graph
-  if (nc > 1 & iso == 0) {s <- rowSums(eig.x$vectors[, 1:nc])} #collecting status scores from separate eigenvectors in disconnected graph
+  if (nc > 1 & nn == 1) {s <- eig.x$vectors[, ci]} #collecting status scores from first eigenvector in disconnected graph with a single connected component
+  if (nc > 1 & nn > 1) {s <- rowSums(eig.x$vectors[, ci])}
   if (norm == "max") {s <- norm.max(s)} #normalizing so that maximum value is one
   if (norm == "one") {s <- norm.one(s)} #normalizing so that vector sums to one
   if (norm == "abm") {s <- norm.abm(s)} #normalizing so that maximum value is one
@@ -31,11 +34,14 @@ distinction <- function(x, norm = "max", digits  = 4) {
     ed <- ecount(xd) == 0 #checking for empty node-deleted subgraph
     nd <- vcount(xd) #number of nodes of node delete subgraph
     ncd <- components(xd)$no #number of components of node-deleted subgraph
-    iso <- sum(components(g)$csize == 1) #checking for isolated components
     eig.xd <- eigen(as_adjacency_matrix(xd)) #eigenvector decomposition of node-deleted subgraph
-    if (ncd == 1) {sd <- eig.xd$vectors[, 1]} #status scores for connected node-deleted subgraph
-    if (ncd > 1 & iso == 0) {sd <- rowSums(eig.xd$vectors[, 1:ncd])} #status scores for disconnected node-deleted subgraph
-    if (ncd > 1 & iso > 0) {sd <- eig.xd$vectors[, 1]} #status scores for disconnected node-deleted subgraph with isolated components
+    if (ncd > 1) {
+      nnd <- sum(components(xd)$csize != 1) #number of non-isolated components in graph
+      cid <- which(components(xd)$csize != 1) #column indices for non-isolated components
+      }
+    if (ncd == 1) {sd <- eig.xd$vectors[, 1]} #collecting status scores from first eigenvector in connected graph
+    if (ncd > 1 & nnd == 1) {sd <- eig.xd$vectors[, cid]} #collecting status scores from first eigenvector in disconnected graph with a single connected component
+    if (ncd > 1 & nnd > 1) {sd <- rowSums(eig.xd$vectors[, cid])}
     if (ed == 0 & nd > 1 & norm == "max") {sd <- norm.max(sd)} #non-empty node-deleted subgraph
     if (ed == 0 & nd > 1 & norm == "one") {sd <- norm.one(sd)} #non-empty node-deleted subgraph
     if (ed == 0 & nd > 1 & norm == "abm") {sd <- norm.abm(sd)} #non-empty node-deleted subgraph
