5268db45646c393a0b3ff2490e46401a6f4bdcb1
Omar Lizardo <omarlizardo@gmail.com>
2026-06-23 15:13:23 -0700

    borf

--- Functions/tab.cent.R
+++ Functions/tab.cent.R
@@ -2,12 +2,29 @@ tab.cent <- function(x,
       d = 3, 
       name = "name", 
       caption = "", 
-      label = "label",
-      rownames) { 
+      label = "label"
+      ) { 
 library(kableExtra)
 library(dplyr)
-x <- distinct(x[, 3:6])
-x <- cbind(rownames, x)
+w <- distinct(x[, 3:6])
+z <- w[, 1]
+for (i in 1:length(z)) {
+      if (i == 1) {
+      e <- as.character(subset(x, scd == z[i])[, 1])
+      if (length(e) > 1) {
+            e <- paste(e, collapse = ", ")
+            }
+      l <- e
+      }
+      if (i > 1) {
+      e <- as.character(subset(x, scd == z[i])[, 1])
+      if (length(e) > 1) {
+            e <- paste(e, collapse = ", ")
+            }
+      l <- c(l, e)
+      }
+   }
+x <- cbind(l, w)
 t <- kbl(x, 
       format = "latex", booktabs = TRUE, linesep = "",
       digits = d, row.names = FALSE,
