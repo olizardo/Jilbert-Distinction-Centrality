96d52a6b6f36b879aa1d7480a974f249d2456664
Omar Lizardo <omarlizardo@gmail.com>
2026-07-20 16:50:40 -0700

    goofa

--- Functions/plot.graph.norm.R
+++ Functions/plot.graph.norm.R
@@ -21,8 +21,11 @@ plot.graph.norm <- function(x, l = "kk", vs = 12, ts = 5, c = 0.05, n = "abm", d
     geom_edge_link(color = "gray50", edge_width = 1)
   
   if (is_real_network) {
-    p <- p + geom_node_point(aes(fill = scd), shape = 21, size = vs, color = "black") +
-         geom_node_text(aes(label = name), color = "black", size = ts, fontface = "bold", repel = TRUE) +
+    # Use smaller sizes for real empirical networks to avoid overlapping
+    vs_real <- vs * 0.5
+    ts_real <- ts * 0.7
+    p <- p + geom_node_point(aes(fill = scd), shape = 21, size = vs_real, color = "black") +
+         geom_node_text(aes(label = name), color = "black", size = ts_real, fontface = "bold", repel = TRUE) +
          scale_fill_gradient2(low = "dodgerblue", mid = "gray90", high = "firebrick", name = "Scaled\nDist.")
   } else {
     p <- p + geom_node_point(aes(fill = scd), shape = 21, size = vs, color = "black") +
